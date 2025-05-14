8e144e3cee6521430ba346dbc554eabe8d205882
Myron Moss <myronmoss@MacBook-Air-23.local>
2025-05-14 16:33:43 -0400

    added shadcn and images

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <LocationProvider>
       <body
         className={`${outfit.variable} ${manrope.variable} antialiased`}
